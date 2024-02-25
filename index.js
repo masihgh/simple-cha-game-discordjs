2652f0978eccd9e22812046c3b1bac94adbdd742
masihgh <masihprogrammer@gmail.com>
2024-02-26 00:39:30 +0330

    event handler :fire:

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ for (const folder of commandFolders) {
 	}
 }
 
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });

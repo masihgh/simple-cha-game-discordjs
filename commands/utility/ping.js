15bc0083eb67752a1149df432c43ef38809f0a5a
masihgh <masihprogrammer@gmail.com>
2024-02-27 19:29:57 +0330

    :sparkles: chnage to powerful ping!

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,6 +5,11 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		const startTime = Date.now();
+		await interaction.reply('Pinging...').then(sentMessage => {
+			const endTime = Date.now();
+			const latency = endTime - startTime;
+			sentMessage.edit(`Pong! Latency is ${latency}ms. API Latency is ${Math.round(interaction.client.ws.ping)}ms.`);
+		});
 	},
 };
